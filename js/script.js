9b82aa2cb8702a77a4ed6138a30f6b91da6a9182
siemen <siemensubbaiah1@gmail.com>
2021-06-02 20:13:12 +0530

    theme now stores in local storage!

--- js/script.js
+++ js/script.js
@@ -1,22 +1,65 @@
+window.addEventListener('load', () => {
+  emailjs.init('your_user_id');
+  // localStorage.setItem('theme');
+});
+
 //SELECTORS!
 const logo = document.getElementById('logo2');
 const Mainlogo = document.getElementById('logo1');
 const navItems = document.querySelector('.nav-items');
 const menu = document.querySelector('.menu');
 const navBar = document.querySelector('nav');
+const form = document.querySelector('form');
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const message = document.querySelector('#message');
+
+// EMAILJS - FORM!
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  emailjs
+    .send('service_id', 'template_id', {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    })
+    .then(
+      () => {
+        console.log('SUCCESS!');
+        window.location = 'thanks.html';
+      },
+      (error) => {
+        console.log('FAILED...', error);
+      }
+    );
+  e.target.reset();
+});
 
 //Theme Toggle!
 const toggler = document.querySelector('.switch-btn');
 
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+  if (currentTheme === 'light') {
+    toggler.classList.add('slide');
+    logo.style.display = 'block';
+    Mainlogo.style.display = 'none';
+  }
+}
 toggler.addEventListener('click', () => {
-  document.body.classList.toggle('light');
+  // document.body.classList.toggle('light');
   toggler.classList.toggle('slide');
   if (toggler.classList.contains('slide')) {
     logo.style.display = 'block';
     Mainlogo.style.display = 'none';
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
   } else {
     logo.style.display = 'none';
     Mainlogo.style.display = 'block';
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
   }
 });
 
