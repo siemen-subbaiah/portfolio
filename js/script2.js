cfea1e9db1a8a05f904f3338aeefc10a00d4536f
siemen <siemensubbaiah1@gmail.com>
2021-07-17 17:16:12 +0530

    making public

--- js/script2.js
+++ js/script2.js
@@ -1,37 +1,9 @@
-window.addEventListener('load', () => {
-  emailjs.init('user_2ycGHDtiEG2OA8KCMYXOt');
-});
-
 //SELECTORS!
 const logo = document.getElementById('logo2');
 const Mainlogo = document.getElementById('logo1');
 const navItems = document.querySelector('.nav-items');
 const menu = document.querySelector('.menu');
 const navBar = document.querySelector('nav');
-const form = document.querySelector('form');
-const name = document.querySelector('#name');
-const email = document.querySelector('#email');
-const message = document.querySelector('#message');
-
-// EMAILJS - FORM!
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  emailjs
-    .send('service_x5agap3', 'template_8upsmbs', {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    })
-    .then(
-      () => {
-        console.log('SUCCESS!');
-        window.location = 'thanks.html';
-      },
-      (error) => {
-        console.log('FAILED...', error);
-      }
-    );
-});
 
 //Theme Toggle!
 const toggler = document.querySelector('.switch-btn');
@@ -60,17 +32,6 @@ toggler.addEventListener('click', () => {
     localStorage.setItem('theme', 'dark');
   }
 });
-
-//GSAP
-const timeline = gsap.timeline({
-  defaults: { ease: 'power2.inOut', duration: 1 },
-});
-
-timeline
-  .fromTo('.hero-contents', { x: '-130%' }, { x: '0' })
-  .fromTo('.hero-img', { opacity: 0 }, { opacity: 1 })
-  .fromTo('.about-me h1', { opacity: 0 }, { opacity: 1 }, '<0.1')
-  .fromTo('.about-me p', { opacity: 0 }, { opacity: 1 }, '<0.1');
 
 //AOS
 AOS.init();
